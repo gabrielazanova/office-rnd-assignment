1633106f91446c27612aa1acf208af328f05242a
gabrielazanova <43805164+gabrielazanova@users.noreply.github.com>
2021-10-04 16:52:13 +0300

    Filtering on member name, location and company

--- src/components/MembersTable.js
+++ src/components/MembersTable.js
@@ -2,8 +2,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import '../style/MembersTable.css';
 import React from 'react';
-// import Table from 'react-bootstrap/Table';
+import { Form, Row, Col } from 'react-bootstrap';
 import BootstrapTable from 'react-bootstrap-table-next';
+import filterFactory, { customFilter, FILTER_TYPES } from 'react-bootstrap-table2-filter';
+import Select from 'react-select';
+// import SearchIcon from '@mui/icons-material/Search';
+// import { InputAdornment } from '@material-ui/core';
 // import Moment from 'react-moment';
 
 export default class MembersTable extends React.Component {
@@ -13,6 +17,22 @@ export default class MembersTable extends React.Component {
         this.state = {
             selectedRows: []
         }
+    }
+
+    onMemberNameFilter = null
+    onLocationFilter = null
+    onCompanyFilter = null
+
+    onMemberNameChange = (event) => {
+        this.onMemberNameFilter(event.target.value)
+    }
+
+    onLocationChange = (optionSelected) => {
+        this.onLocationFilter(optionSelected.value);
+    }
+
+    onCompanyChange = (optionSelected) => {
+        this.onCompanyFilter(optionSelected.value);
     }
 
     render() {
@@ -26,9 +46,9 @@ export default class MembersTable extends React.Component {
                     newSelectedRows.push(row._id)
                 }
                 else {
-                    var member = newSelectedRows.indexOf(row._id);
+                    var member = newSelectedRows.indexOf(row._id)
                     if (member !== -1) {
-                        newSelectedRows.splice(member, 1);
+                        newSelectedRows.splice(member, 1)
                     }
                 }
                 this.setState({ selectedRows: newSelectedRows })
@@ -47,15 +67,42 @@ export default class MembersTable extends React.Component {
                 }
                 this.props.onMembersToBeDeleted(this.state.selectedRows)
             }
-        };
+        }
 
-        const members = this.props.members;
+        const locationOptions = [
+            { value: '', label: 'All' },
+            { value: '5d81dcfcb18f3e0010f50407', label: '5d81dcfcb18f3e0010f50407' },
+            { value: '5d839686e59be52704c30fbb', label: 'Strawberry' }
+        ]
+
+        const companyOptions = [
+            { value: '', label: 'All' },
+            { value: '5d837099d4809b00104883ac', label: '5d837099d4809b00104883ac' },
+            { value: '5d8374f9d4809b00104883ee', label: '5d8374f9d4809b00104883ee' },
+            { value: '5d8381c6d4809b0010495ffa', label: '5d8381c6d4809b0010495ffa' },
+            { value: '5d81dcfcb18f3e0010f5040d', label: '5d81dcfcb18f3e0010f5040d' }
+        ]
+
+        const members = this.props.members
         const columns = [{
             dataField: 'name',
-            text: 'MEMBER'
+            text: 'MEMBER',
+            filter: customFilter(),
+            filterRenderer: (onFilter) => {
+                this.onMemberNameFilter = onFilter
+                return null
+            },
         }, {
             dataField: 'team',
-            text: 'TEAM'
+            text: 'TEAM',
+            filter: customFilter({
+                type: FILTER_TYPES.SELECT,
+                options: companyOptions
+            }),
+            filterRenderer: (onFilter) => {
+                this.onCompanyFilter = onFilter
+                return null
+            }
         }, {
             dataField: 'calculatedStatus',
             text: 'STATUS/LABEL'
@@ -65,23 +112,59 @@ export default class MembersTable extends React.Component {
             sort: true
         }, {
             dataField: 'office',
-            text: 'LOCATION'
-        }];
+            text: 'LOCATION',
+            filter: customFilter({
+                type: FILTER_TYPES.SELECT,
+                options: locationOptions
+            }),
+            filterRenderer: (onFilter) => {
+                this.onLocationFilter = onFilter
+                return null
+            }
+        }]
 
         return (
             <div className="container-fluid membersTable">
-                <div className="row">
+                <Row>
+                    <Col xs={6} md={2}>
+                        <Form>
+                            <Form.Group className="mb-3" >
+                                {/* <InputAdornment position="start">
+                                    <SearchIcon />
+                                </InputAdornment> */}
+                                <Form.Control type="text" onChange={this.onMemberNameChange}/>
+                            </Form.Group>
+                        </Form>
+                    </Col>
+                    <Col xs={6} md={3}>
+                        <Select
+                            onChange={this.onLocationChange}
+                            options={locationOptions}
+                            placeholder='LOCATION: '
+                            closeMenuOnSelect={true} />
+                    </Col>
+                    <Col xs={6} md={3}>
+                        <Select
+                            onChange={this.onCompanyChange}
+                            options={companyOptions}
+                            placeholder='COMPANY: '
+                            closeMenuOnSelect={true} />
+                    </Col>
+                </Row>
+                <Row>
                     {this.props.members.length > 0 ?
                         <BootstrapTable
+                            bootstrap4
                             keyField='_id'
                             selectRow={selectRow}
                             data={members}
                             columns={columns}
-                            bordered={false} /> :
+                            bordered={false}
+                            filter={filterFactory()} /> :
                         <div>No data</div>
                     }
-                </div>
+                </Row>
             </div>
-        );
+        )
     }
 }
